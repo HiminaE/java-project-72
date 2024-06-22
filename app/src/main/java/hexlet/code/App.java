239718b8f92309f4528435c6967af25b196ecbb3
HiminaE <himinaev@yandex.ru>
2024-06-22 13:41:01 +0300

    Checkstyle add

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -3,12 +3,24 @@
  */
 package hexlet.code;
 
+import io.javalin.Javalin;
+import io.javalin.rendering.template.JavalinJte;
+
 public class App {
-    public String getGreeting() {
-        return "Hello World!";
+    private static int getPort() {
+        String port = System.getenv().getOrDefault("PORT", "7070");
+        return Integer.parseInt(port);
+    }
+    public static Javalin getApp() {
+        var app = Javalin.create(config -> {
+            config.bundledPlugins.enableDevLogging();
+            config.fileRenderer(new JavalinJte());
+        });
+        return app;
     }
 
     public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
+        Javalin app = getApp();
+        app.start(getPort());
     }
 }
