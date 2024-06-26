3929ecc6eefe79a7d9ffb06a8b80c4b1e6d0b5a9
HiminaE <himinaev@yandex.ru>
2024-06-26 19:04:07 +0300

    model add

---
+++ app/src/main/java/hexlet/code/model/Url.java
@@ -0,0 +1,20 @@
+package hexlet.code.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.sql.Timestamp;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class Url {
+    private Long id;
+    private String name;
+    private Timestamp createdAt;
+
+    public Url(String name) {
+        this.name = name;
+    }
+}
