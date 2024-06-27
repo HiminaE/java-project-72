f06115132a3767baf01f6dcdaaea220a09fd759d
HiminaE <himinaev@yandex.ru>
2024-06-27 18:50:27 +0300

    Correct App

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -26,15 +26,29 @@ import java.util.stream.Collectors;
 
 @Slf4j
 public class App {
-    private static int getPort() {
-        String port = System.getenv().getOrDefault("PORT", "7070");
-        return Integer.parseInt(port);
+    public static void main(String[] args) throws SQLException, IOException {
+        var app = getApp();
+        app.start(getPort());
     }
-    public static Javalin getApp() {
+
+    public static Javalin getApp() throws IOException, SQLException {
         var app = Javalin.create(config -> {
             config.bundledPlugins.enableDevLogging();
-            config.fileRenderer(new JavalinJte());
+            config.fileRenderer(new JavalinJte(createTemplateEngine()));
         });
+
+        var hikariConfig = new HikariConfig();
+        hikariConfig.setJdbcUrl(getDataBaseUrl());
+
+        var dataSource = new HikariDataSource(hikariConfig);
+        var sql = readResourceFile("schema.sql");
+
+        try (var connection = dataSource.getConnection();
+             var statement = connection.createStatement()) {
+            statement.execute(sql);
+        }
+
+        BaseRepository.dataSource = dataSource;
 
         app.before(ctx -> {
             ctx.contentType("text/html; charset=utf-8");
@@ -45,7 +59,18 @@ public class App {
         app.get(Paths.urlsPath(), UrlController::showAddedUrls);
         app.get(Paths.urlsIdPathWithoutId(), UrlController::showInfoAboutUrl);
         app.post(Paths.urlsIdChecksPathWithoutId(), UrlCheckController::makeCheck);
+
         return app;
+    }
+
+    public static int getPort() {
+        String port = System.getenv().getOrDefault("PORT", "7070");
+        return Integer.parseInt(port);
+    }
+
+    public static String getDataBaseUrl() {
+        return System.getenv().getOrDefault("JDBC_DATABASE_URL",
+                "jdbc:h2:mem:project;DB_CLOSE_DELAY=-1;");
     }
 
     private static String readResourceFile(String fileName) throws IOException {
@@ -59,11 +84,5 @@ public class App {
         ClassLoader classLoader = App.class.getClassLoader();
         ResourceCodeResolver codeResolver = new ResourceCodeResolver("templates", classLoader);
         return TemplateEngine.create(codeResolver, ContentType.Html);
-    }
-
-    public static void main(String[] args) throws SQLException, IOException {
-        Javalin app = getApp();
-        app.get("/", ctx -> ctx.result("Hello World"));
-        app.start(getPort());
     }
 }
