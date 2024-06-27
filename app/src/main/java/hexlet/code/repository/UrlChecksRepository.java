0e387126b803624f74731babda02be78b8238013
HiminaE <himinaev@yandex.ru>
2024-06-27 17:36:54 +0300

    UrlRepo add

---
+++ app/src/main/java/hexlet/code/repository/UrlChecksRepository.java
@@ -0,0 +1,89 @@
+package hexlet.code.repository;
+
+import hexlet.code.model.UrlCheck;
+import lombok.SneakyThrows;
+
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class UrlChecksRepository extends BaseRepository {
+    @SneakyThrows
+    public static List<UrlCheck> getUrlChecksByUrlId(Long id) {
+        var result = new ArrayList<UrlCheck>();
+        var sql = "SELECT * FROM url_checks WHERE url_id = ?";
+        try (var conn = dataSource.getConnection();
+             var prepareStmt = conn.prepareStatement(sql)) {
+            prepareStmt.setLong(1, id);
+            var resultSet = prepareStmt.executeQuery();
+            while (resultSet.next()) {
+                var urlCheck = UrlCheck.builder()
+                        .id(resultSet.getLong("id"))
+                        .description(resultSet.getString("description"))
+                        .h1(resultSet.getString("h1"))
+                        .title(resultSet.getString("title"))
+                        .createdAt(resultSet.getTimestamp("created_at"))
+                        .urlId(id)
+                        .statusCode(resultSet.getInt("status_code"))
+                        .build();
+                result.add(urlCheck);
+            }
+
+        }
+
+        return result;
+    }
+
+    public static void save(UrlCheck urlCheck) throws SQLException {
+        var sql = "INSERT INTO url_checks (status_code, title, h1, description, url_id, created_at) "
+                + "VALUES (?, ?, ?, ?, ?, ?)";
+        var time = new Timestamp(System.currentTimeMillis());
+        try (var conn = dataSource.getConnection();
+             var prepareStmt = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
+            prepareStmt.setInt(1, urlCheck.getStatusCode());
+            prepareStmt.setString(2, urlCheck.getTitle());
+            prepareStmt.setString(3, urlCheck.getH1());
+            prepareStmt.setString(4, urlCheck.getDescription());
+            prepareStmt.setLong(5, urlCheck.getUrlId());
+            prepareStmt.setTimestamp(6, time);
+            prepareStmt.executeUpdate();
+
+            var generatedKeys = prepareStmt.getGeneratedKeys();
+            if ((generatedKeys.next())) {
+                urlCheck.setId(generatedKeys.getLong(1));
+                urlCheck.setCreatedAt(time);
+            }
+        }
+    }
+
+    @SneakyThrows
+    public static Map<Long, UrlCheck> getLastChecks() {
+        var result = new HashMap<Long, UrlCheck>();
+        var sql = "SELECT uc.url_id, uc.status_code, uc.created_at "
+                + "FROM url_checks uc "
+                + "JOIN ( "
+                + "    SELECT url_id, MAX(created_at) AS max_created_at"
+                + "    FROM url_checks "
+                + "    GROUP BY url_id "
+                + ") uc_max ON uc.url_id = uc_max.url_id AND uc.created_at = uc_max.max_created_at;";
+        try (var conn = dataSource.getConnection();
+             var prepareStmt = conn.prepareStatement(sql)) {
+            var resultSet = prepareStmt.executeQuery();
+            while (resultSet.next()) {
+                var urlCheck = UrlCheck.builder()
+                        .createdAt(resultSet.getTimestamp("created_at"))
+                        .urlId(resultSet.getLong("url_id"))
+                        .statusCode(resultSet.getInt("status_code"))
+                        .build();
+                result.put(urlCheck.getUrlId(), urlCheck);
+            }
+
+        }
+
+        return result;
+    }
+}
---
+++ app/src/main/java/hexlet/code/repository/UrlsRepository.java
@@ -0,0 +1,88 @@
+package hexlet.code.repository;
+
+import hexlet.code.model.Url;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.SneakyThrows;
+
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Getter
+@Setter
+public class UrlsRepository extends BaseRepository {
+    public static void save(Url url) throws SQLException {
+        var sql = "INSERT INTO urls (name, created_at) VALUES (?, ?)";
+        var time = new Timestamp(System.currentTimeMillis());
+        try (var conn = dataSource.getConnection();
+             var prepareStmt = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
+            prepareStmt.setString(1, url.getName());
+            prepareStmt.setTimestamp(2, time);
+            prepareStmt.executeUpdate();
+
+            var generatedKey = prepareStmt.getGeneratedKeys();
+            if (generatedKey.next()) {
+                url.setId(generatedKey.getLong(1));
+                url.setCreatedAt(time);
+            }
+        }
+    }
+
+    public static List<Url> getUrls() throws SQLException {
+        var sql = "SELECT * FROM urls";
+        try (var conn = dataSource.getConnection();
+             var stmt = conn.prepareStatement(sql)) {
+            var resultSet = stmt.executeQuery();
+            var result = new ArrayList<Url>();
+            while (resultSet.next()) {
+                var id = resultSet.getLong("id");
+                var name = resultSet.getString("name");
+                var createdAt = resultSet.getTimestamp("created_at");
+                var site = new Url(id, name, createdAt);
+                result.add(site);
+            }
+            return result;
+        }
+    }
+
+    public static Optional<Url> getById(Long id) throws SQLException {
+        var sql = "SELECT * FROM urls WHERE id = ?";
+        try (var conn = dataSource.getConnection();
+             var stmt = conn.prepareStatement(sql)) {
+            stmt.setLong(1, id);
+            var resultSet = stmt.executeQuery();
+            if (resultSet.next()) {
+                var name = resultSet.getString("name");
+                var createdAt = resultSet.getTimestamp("created_at");
+                var site = new Url(id, name, createdAt);
+                return Optional.of(site);
+            } else {
+                return Optional.empty();
+            }
+        } catch (SQLException e) {
+            throw new SQLException(e.getMessage());
+        }
+    }
+
+    @SneakyThrows
+    public static Optional<Url> getByName(String name) {
+        var sql = "SELECT * FROM urls WHERE name = ?";
+        try (var conn = dataSource.getConnection();
+             var stmt = conn.prepareStatement(sql)) {
+            stmt.setString(1, name);
+            var resultSet = stmt.executeQuery();
+            if (resultSet.next()) {
+                var id = resultSet.getLong("id");
+                var createdAt = resultSet.getTimestamp("created_at");
+                var site = new Url(id, name, createdAt);
+                return Optional.of(site);
+            } else {
+                return Optional.empty();
+            }
+        }
+    }
+}
