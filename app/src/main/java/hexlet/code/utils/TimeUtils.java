caf073b7d953e2f4376486fae08acaa89c2c5354
HiminaE <155444135+HiminaE@users.noreply.github.com>
2024-07-24 15:22:25 +0300

    Update TimeUtils.java

--- app/src/main/java/hexlet/code/utils/TimeUtils.java
+++ app/src/main/java/hexlet/code/utils/TimeUtils.java
@@ -1,10 +1,14 @@
 package hexlet.code.utils;
 
 import java.sql.Timestamp;
+import java.time.Instant;
 import java.text.SimpleDateFormat;
 
 public class TimeUtils {
     public static String getFormattedData(Timestamp timestamp) {
         return new SimpleDateFormat("dd/MM/yyyy HH:mm").format(timestamp);
+    }
+    public static String getFormattedData(Instant instant) {
+        return new SimpleDateFormat("dd/MM/yyyy HH:mm").format(instant);
     }
 }
