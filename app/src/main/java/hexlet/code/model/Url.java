6e14de6a0189c5e02d1121d6f56acbcef3f4e50b
HiminaE <155444135+HiminaE@users.noreply.github.com>
2024-07-24 12:36:16 +0300

    Update Url.java

--- app/src/main/java/hexlet/code/model/Url.java
+++ app/src/main/java/hexlet/code/model/Url.java
@@ -13,13 +13,14 @@ public class Url {
     private Long id;
     private String name;
     private Instant createdAt;
-    
+        
     public Url(String name, Instant createdAt) {
         this.name = name;
         this.createdAt = createdAt;        
     }
-
+    
     public Url(String name) {
         this.name = name;
     }
+    
 }
