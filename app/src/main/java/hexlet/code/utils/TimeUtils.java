94b8b2b7c68569cbbb390460c253285bac3fa561
HiminaE <himinaev@yandex.ru>
2024-07-29 20:08:59 +0300

    Corrected TimeUtils

--- app/src/main/java/hexlet/code/utils/TimeUtils.java
+++ app/src/main/java/hexlet/code/utils/TimeUtils.java
@@ -5,9 +5,8 @@ import java.time.ZoneOffset;
 import java.time.format.DateTimeFormatter;
 
 public class TimeUtils {
-    private static final String PATTERN_FORMAT = "dd.MM.yyyy";
     public static String getFormattedData(Instant instant) {
-        String formatted = DateTimeFormatter.ofPattern(PATTERN_FORMAT)
+        String formatted = DateTimeFormatter.ofPattern("dd.MM.yyyy HH:mm")
                 .format(instant.atOffset(ZoneOffset.UTC));
         return formatted;
     }
